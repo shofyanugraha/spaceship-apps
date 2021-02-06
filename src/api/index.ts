17ca5af4c43aaca00c83e3fa548438a517adec6e
rizkynugraha <rizkyshofyan@gmail.com>
2021-02-07 01:40:54 +0700

    add headers

--- src/api/index.ts
+++ src/api/index.ts
@@ -3,3 +3,4 @@ import axios from "axios";
 axios.defaults.baseURL = process.env.VUE_APP_API_BASE_URL;
 axios.defaults.headers.post["Content-Type"] = "application/json";
 axios.defaults.headers.post["Accept"] = "application/json";
+axios.defaults.headers.post["Access-Control-Allow-Origin"] = "*";
