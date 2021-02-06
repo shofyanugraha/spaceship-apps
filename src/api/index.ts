3737279816300b73c928a4560efbfa8d870cc2b2
rizkynugraha <rizkyshofyan@gmail.com>
2021-02-07 01:45:51 +0700

    add proxy

--- src/api/index.ts
+++ src/api/index.ts
@@ -4,3 +4,8 @@ axios.defaults.baseURL = process.env.VUE_APP_API_BASE_URL;
 axios.defaults.headers.post["Content-Type"] = "application/json";
 axios.defaults.headers.post["Accept"] = "application/json";
 axios.defaults.headers.post["Access-Control-Allow-Origin"] = "*";
+axios.defaults.proxy = {
+  host: "localhost",
+  port: 8081,
+  protocol: "https"
+};
